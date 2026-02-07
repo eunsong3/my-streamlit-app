e79fda5c53ac46feecb630b29f8e01e7194a3c36
eunsong3 <eunsong3@yonsei.ac.kr>
2026-02-08 04:19:16 +0900

    Update recommender.py

--- recommender.py
+++ recommender.py
@@ -1,18 +1,18 @@
 # recommender.py
 
-from data import PLANS
-
-def recommend_plans(user):
+def recommend_plans(user, plans):
     scored = []
 
-    for plan in PLANS:
+    for plan in plans:
         score = 0
+
         if plan["price"] <= user["budget"]:
-            score += 3
+            score += 5
         if plan["data_gb"] >= user["data_usage"]:
-            score += 3
+            score += 5
         if plan["device_support"] == user["device_type"]:
             score += 2
+
         scored.append((score, plan))
 
     scored.sort(key=lambda x: x[0], reverse=True)
