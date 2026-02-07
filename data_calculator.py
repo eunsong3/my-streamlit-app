ee8cfd89b39b4185ee541abdec7f5347dfaa1a85
eunsong3 <eunsong3@yonsei.ac.kr>
2026-02-08 03:44:43 +0900

    Update data_calculator.py

--- data_calculator.py
+++ data_calculator.py
@@ -1,13 +1,8 @@
 # data_calculator.py
 
 def estimate_monthly_data(hours_per_day, apps, heavy_download):
-    """
-    간단한 휴리스틱 기반 추정 (MVP용)
-    """
-
     data = 0
 
-    # 사용 시간 기준 (GB/월)
     if hours_per_day < 1:
         data += 3
     elif hours_per_day < 3:
@@ -17,7 +12,6 @@ def estimate_monthly_data(hours_per_day, apps, heavy_download):
     else:
         data += 25
 
-    # 앱 사용
     if "YouTube" in apps:
         data += 10
     if "Netflix" in apps:
@@ -25,7 +19,6 @@ def estimate_monthly_data(hours_per_day, apps, heavy_download):
     if "Instagram" in apps:
         data += 5
 
-    # 다운로드 여부
     if heavy_download:
         data += 10
 
