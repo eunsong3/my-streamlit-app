95b382a3ddadc0ec9c0027be5cc58fe70c107e85
eunsong3 <eunsong3@yonsei.ac.kr>
2026-02-08 03:07:49 +0900

    Create data.py

---
+++ data.py
@@ -0,0 +1,36 @@
+# data.py
+
+PLANS = [
+    {
+        "name": "알뜰폰 데이터 10GB",
+        "price": 15000,
+        "data_gb": 10,
+        "carrier": "MVNO",
+        "device_support": "자급제",
+        "ott_free": []
+    },
+    {
+        "name": "LTE 실속 요금제",
+        "price": 33000,
+        "data_gb": 30,
+        "carrier": "KT",
+        "device_support": "자급제",
+        "ott_free": []
+    },
+    {
+        "name": "5G 청년 요금제",
+        "price": 55000,
+        "data_gb": 100,
+        "carrier": "SKT",
+        "device_support": "공시지원금",
+        "ott_free": ["Wavve"]
+    },
+    {
+        "name": "외국인 전용 요금제",
+        "price": 25000,
+        "data_gb": 20,
+        "carrier": "LG U+",
+        "device_support": "단기",
+        "ott_free": []
+    }
+]
