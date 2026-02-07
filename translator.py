b58a38036f2c55b6b9c1bbf80164ed1f7bfa7c1a
eunsong3 <eunsong3@yonsei.ac.kr>
2026-02-08 04:18:32 +0900

    Update translator.py

--- translator.py
+++ translator.py
@@ -2,22 +2,16 @@
 
 import requests
 
-DEEPL_API_URL = "https://api-free.deepl.com/v2/translate"
+DEEPL_URL = "https://api-free.deepl.com/v2/translate"
 
 def translate(text, target_lang, api_key):
     if not api_key or target_lang == "KO":
         return text
 
-    headers = {
-        "Authorization": f"DeepL-Auth-Key {api_key}"
-    }
-
-    data = {
-        "text": text,
-        "target_lang": target_lang
-    }
-
-    response = requests.post(DEEPL_API_URL, headers=headers, data=data)
-    response.raise_for_status()
-
-    return response.json()["translations"][0]["text"]
+    res = requests.post(
+        DEEPL_URL,
+        headers={"Authorization": f"DeepL-Auth-Key {api_key}"},
+        data={"text": text, "target_lang": target_lang}
+    )
+    res.raise_for_status()
+    return res.json()["translations"][0]["text"]
