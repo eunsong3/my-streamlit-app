dfc18cb8cf79d10b46dbee57d0044ef2f8eae4a1
eunsong3 <eunsong3@yonsei.ac.kr>
2026-02-08 14:26:30 +0900

    Update recommender.py

--- recommender.py
+++ recommender.py
@@ -5,24 +5,17 @@ def recommend_plans(user, plans):
 
     for p in plans:
         score = 0
-
         if p["price"] <= user["budget"]:
             score += 5
         if p["data_gb"] >= user["data_usage"]:
             score += 5
 
-        # 시나리오 가중치
-        if user["scenario"] == "외국인 유학생":
-            if p["price"] < 35000:
-                score += 2
-
-        if user["scenario"] == "경제적 자립 준비 학생":
-            if p["price"] < 30000:
-                score += 3
-
-        if user["scenario"] == "기기 교체 희망 학생":
-            if p["data_gb"] >= 30:
-                score += 2
+        if user["scenario"] == "외국인 유학생" and p["price"] < 35000:
+            score += 2
+        if user["scenario"] == "경제적 자립 준비 학생" and p["price"] < 30000:
+            score += 3
+        if user["scenario"] == "기기 교체 희망 학생" and p["data_gb"] >= 30:
+            score += 2
 
         scored.append((score, p))
 
